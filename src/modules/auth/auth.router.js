cca61d83500637ac65ae4ae0e9fb04c6aacd90ff
AmanSalman <rapamansalman@gmail.com>
2025-06-27 19:28:19 +0300

    add Bonus Features

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -1,5 +1,7 @@
 // ===== Auth Routes =====
-// POST   /auth/register     --> Register a new user
-// POST   /auth/login        --> Login and get a JWT token
-// POST   /auth/confirm      --> Confirm email
-// POST   /auth/resend-code  --> Resend email confirmation code (optional)
+// POST   /auth/register       --> Register a new user
+// POST   /auth/login          --> Login and get a JWT token
+// POST   /auth/confirm        --> Confirm email
+// POST   /auth/resend-code    --> Resend email confirmation code (optional)
+// POST   /auth/forgot-password --> Send reset code to email (💡 Bonus)
+// POST   /auth/reset-password  --> Reset password using code (💡 Bonus)
